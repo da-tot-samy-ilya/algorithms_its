7a5bfe8dd8ceddaf65651d494da32176f01c530e
ilya <da.tot.samy.ilya@gmail.com>
2022-08-09 22:22:27 +0500

    readme

--- node/a.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let inFile = fs.readFileSync('a.txt', "utf8");
-inFile = inFile.split('\n');
-let commands = [];
-for (i of inFile) {
-    k = i.split(' ');
-    commands.push(k);
-}
-console.log(commands);
